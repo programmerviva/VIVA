04c3959a9417f9dfa12d4b1ed18d7349dde226dd
Vikas Vaibhav <sinhaamanas2832439@gmail.com>
2025-01-31 19:25:32 +0530

    pages

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,13 @@
+import { Container as ContainerComponent, PostForm } from "../components"
+
+const AddPost = () => {
+  return (
+    <div className="py-8">
+      <ContainerComponent>
+        <PostForm />
+      </ContainerComponent>
+    </div>
+  )
+}
+
+export default AddPost
---
+++ src/pages/AllPost.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { Container, PostCard } from "../components";
+import appwriteService from "../appwrite/config";
+const AllPost = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {}, []);
+  appwriteService.getPosts([]).then((posts) => 
+  {
+    if(posts) {
+      setPosts(posts.documents)
+    }
+  });
+
+  return <div className="w-full py-8"> 
+      <Container>
+        {/* 
+        // direct methods
+        {posts.map((post) => (
+          <PostCard key={post.$id} post={post}/>
+        ))} */}
+
+        <div className="flex flex-wrap">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-1/4">
+                <PostCard post={post}/>
+              </div>
+            ))}
+        </div>
+      </Container>
+  </div>;
+};
+
+export default AllPost;
