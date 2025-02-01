d24da2eedcd9496b21e5e9109852db4aacc08da1
Vikas Vaibhav <sinhaamanas2832439@gmail.com>
2025-02-01 14:39:21 +0530

    debugging

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -36,7 +36,7 @@ function Header() {
   ];
 
   return (
-    <header className="py-3 shadow bg-gray-500">
+    <header className="py-3 shadow bg-gray-900 text-white">
       <Container>
         <nav className="flex">
           <div className="mr-4">
@@ -44,13 +44,13 @@ function Header() {
               <Logo width="70px" />
             </Link>
           </div>
-          <ul className="flex ml-auto">
+          <ul className="flex ml-auto ">
             {navItems.map((item) =>
               item.active ? (
                 <li key={item.name}>
                   <button
                     onClick={() => navigate(item.slug)}
-                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 hover:text-gray-900 rounded-full"
                   >
                     {item.name}
                   </button>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 
+
 function Logo({ width = "100px" }) {
-  return <div style={{ width }}>Logo</div>;
+  return <div style={{ width }}>logo</div>;
 }
 
 Logo.propTypes = {
